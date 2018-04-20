8c8892ca38d774e885518fc911ac6b10237991ff
ezwelty <ethan.welty@gmail.com>
2018-04-20 14:55:29 -0600

    Add seperate script for updating BCF Orders sheet

---
+++ update_orders.R
@@ -0,0 +1,24 @@
+library(magrittr)
+source("functions.R")
+source("passwords.R")
+
+# Read all orders
+token <- pma_login(pma_user, pma_pass)
+tax_exempt_users <- c("saramolton16@gmail.com", "bonaishalom")
+orders <- get_orders(overwrite = TRUE, token = token) %>%
+  dplyr::mutate(
+    tax_exempt = user_id %in% tax_exempt_users,
+    markup = sales - price_paid
+  ) %>%
+  dplyr::filter(
+    account_id != "bcf_internal"
+  )
+
+# Save new rows to file
+orders %>%
+  dplyr::select(foodclub, order_date, account_id, user_id, price_paid, tax_paid, collected, sales, tax, food, tax_exempt) %>%
+  dplyr::arrange(order_date, account_id, user_id) %>%
+  dplyr::filter(order_date >= as.Date("2017-12-24")) %>%
+  write.table("new_orders.tsv", quote = FALSE, na = "", sep = "\t", row.names = FALSE, col.names = FALSE)
+
+# Copy-paste new_orders.tsv into https://docs.google.com/spreadsheets/d/1UCTvRndnzphGTGPRsSX9Y6FuuZhMIMlF7XRTb1jGzk4/edit#gid=1616631951
