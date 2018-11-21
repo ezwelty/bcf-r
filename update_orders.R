a1c8a832db3ed80c67034b160ea73fb33ff1bfce
ezwelty <ethan.welty@gmail.com>
2018-11-21 12:55:04 -0700

    Update orders update script

--- update_orders.R
+++ update_orders.R
@@ -6,7 +6,7 @@ source("passwords.R")
 token <- pma_login(pma_user, pma_pass)
 # tax_exempt_users <- c("saramolton16@gmail.com", "bonaishalom")
 tax_exempt_users <- c()
-orders <- get_orders(overwrite = FALSE, token = token) %>%
+orders <- get_orders(overwrite = TRUE, token = token) %>%
   dplyr::mutate(
     tax_exempt = user_id %in% tax_exempt_users
   ) %>%
@@ -18,7 +18,7 @@ orders <- get_orders(overwrite = FALSE, token = token) %>%
 orders %>%
   dplyr::select(foodclub, order_date, account_id, user_id, price_paid, tax_paid, collected, sales, tax, food, tax_exempt) %>%
   dplyr::arrange(order_date, account_id, user_id) %>%
-  dplyr::filter(order_date >= as.Date("2018-08-01")) %>%
+  dplyr::filter(order_date > as.Date("2018-09-16")) %>%
   write.table("new_orders.tsv", quote = FALSE, na = "", sep = "\t", row.names = FALSE, col.names = FALSE)
 
 # Copy-paste new_orders.tsv into https://docs.google.com/spreadsheets/d/1UCTvRndnzphGTGPRsSX9Y6FuuZhMIMlF7XRTb1jGzk4/edit#gid=1616631951
